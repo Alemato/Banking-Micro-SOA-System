c0a529b6a0c7e01bab743c8b2696db3dbb2a6a27
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2024-06-28 11:18:42 +0200

    init servizio AccountService di tipo REST Producer

---
+++ AccountService/src/test/java/it/univaq/sose/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.sose.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
