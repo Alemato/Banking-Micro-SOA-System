c04e5f77236fedff68fec0c15366f4deb0c95367
Stefano Decina <stefano.decina1@gmail.com>
2024-07-03 09:29:02 +0200

    Enable JPA Auditing

--- bank-account-service/src/main/java/it/univaq/sose/bankaccountservice/BankAccountServiceApplication.java
+++ bank-account-service/src/main/java/it/univaq/sose/bankaccountservice/BankAccountServiceApplication.java
@@ -2,7 +2,9 @@ package it.univaq.sose.bankaccountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BankAccountServiceApplication {
 
