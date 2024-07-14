51c37ce7e0049a0800d556df1346763809bd4ad5
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2024-07-14 09:07:21 +0200

    aggiornamento specifiche

--- loan-service-prosumer/src/main/java/it/univaq/sose/loanserviceprosumer/service/LoanService.java
+++ loan-service-prosumer/src/main/java/it/univaq/sose/loanserviceprosumer/service/LoanService.java
@@ -1,6 +1,7 @@
 package it.univaq.sose.loanserviceprosumer.service;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -62,8 +63,8 @@ public interface LoanService {
                             responseCode = "200",
                             description = "Loans Retrieved Successfully",
                             content = {
-                                    @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = LoanDto.class)),
-                                    @Content(mediaType = MediaType.APPLICATION_XML, schema = @Schema(implementation = LoanDto.class))
+                                    @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = LoanDto.class))),
+                                    @Content(mediaType = MediaType.APPLICATION_XML, array = @ArraySchema(schema = @Schema(implementation = LoanDto.class)))
                             }),
                     @ApiResponse(
                             responseCode = "404",
